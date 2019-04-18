97a6029c463acc3fdcf9a32946468bb4de3c06bd
joaoteixeira <joaoteixeira@joaoteixeira-X555LJ>
2019-04-18 17:49:47 +0100

    send encripted message method

--- src/utils/Test.java
+++ src/utils/Test.java
@@ -1,9 +1,19 @@
 package utils;
 
+import java.net.DatagramPacket;
+import java.net.InetSocketAddress;
+
 public class Test {
 	public static void main(String[] args) throws Exception {
 		// System.out.println(secDatagramSockets.ciphersuitReader());
 		ciphersuiteConfig cs = new ciphersuiteConfig();
 		System.out.println(cs.toString());
+		secDatagramSockets sec = new secDatagramSockets();
+		byte[] buff = new byte[65000];
+		InetSocketAddress addr = new InetSocketAddress("localhost", 1233);
+		DatagramPacket p = new DatagramPacket(buff, buff.length, addr);
+		String data = "asdasdasdasdasdasdsa";
+		p.setData(data.getBytes(), 0, data.getBytes().length);
+		sec.send(p);
 	}
 }
--- src/utils/secDatagramSockets.java
+++ src/utils/secDatagramSockets.java
@@ -8,6 +8,7 @@ import java.net.SocketException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Random;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -31,9 +32,21 @@ public class secDatagramSockets extends DatagramSocket {
 		fastControlMACKey = new SecretKeySpec(new byte[2], "AES"); // TODO: Mudar para shared key
 		MAC_Key = new SecretKeySpec(new byte[2], "AES");
 		// TODO Auto-generated constructor stub
+
 	}
 
 	public void send(DatagramPacket datagram) {
+		Random random = new Random();
+		long id = random.nextLong();
+		long nonce = random.nextLong();
+		String message = id + nonce + new String(datagram.getData());
+		System.out.println("datagram content: " + new String(datagram.getData()));
+		try {
+			System.out.println("message: " + message);
+			System.out.println(new String(createHashFromByteArray(message.getBytes())));
+		} catch (Exception e) {
+
+		}
 		// Pegar mensagem
 		// String message = encrypt();
 		// Encriptar
@@ -90,16 +103,24 @@ public class secDatagramSockets extends DatagramSocket {
 		return null;
 	}
 
-	private byte[] createFastControlHashFromByteArray(byte[] message) {
+	private byte[] createFastControlHashFromByteArray(byte[] message) throws Exception {
 		return hashMessage(message, fastControlMACKey);
 	}
 
-	private byte[] createHashFromByteArray(byte[] message) {
+	private byte[] createHashFromByteArray(byte[] message) throws Exception {
 		return hashMessage(message, MAC_Key);
 	}
 
-	private byte[] hashMessage(byte[] message, Key key) {
-		return null;
+	private byte[] hashMessage(byte[] message, Key key) throws Exception {
+		try {
+			Mac mac = Mac.getInstance("HmacSHA1");
+			mac.init(key);
+			return mac.doFinal(message);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("Hash failed");
+			throw new Exception("Hash failed");
+		}
 	}
 
 	private byte[] encryptMessageWithGivenKey(Key key, byte[] message) throws IllegalBlockSizeException,
