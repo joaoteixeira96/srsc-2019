51b19475f41556a5ee52b2a6e22c267bf30d5167
joaoteixeira <joaoteixeira@joaoteixeira-X555LJ>
2019-04-19 12:45:33 +0100

    AESGCMUpdate class added

---
+++ src/utils/AESGCMUpdate.java
@@ -0,0 +1,160 @@
+package utils;
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+import javax.crypto.AEADBadTagException;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+
+public class AESGCMUpdate {
+
+// To use GMACs need some parameters ...
+
+// In the case we will see GMACs w/ AES-GCM parameters
+
+	public static final int AES_KEY_SIZE = 128; // in bits
+
+	public static final int GCM_NONCE_LENGTH = 12; // in bytes
+
+	public static final int GCM_TAG_LENGTH = 16; // in bytes
+
+	public static void main(String[] args) throws Exception {
+
+		int testNum = 0; // pass
+
+		if (args.length > 0) {
+
+			testNum = Integer.parseInt(args[0]);
+
+			if (testNum < 0 || testNum > 3) {
+
+				System.out.println("Usage: java AESGCMUpdateAAD2 [X]");
+
+				System.out.println("X can be 0, 1, 2, 3");
+
+				System.exit(1);
+
+			}
+
+		}
+
+		// Supposing this is the message that we will encrypt/decrypt
+
+		byte[] input = "CSNS Course AES-GCM Hello World!".getBytes();
+
+		// We need to Initialise random and generate key
+
+		SecureRandom random = SecureRandom.getInstanceStrong();
+
+		KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+
+		keyGen.init(AES_KEY_SIZE, random);
+
+		SecretKey key = keyGen.generateKey(); // used as the key
+
+		// Encryption Step
+
+		Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding", "SunJCE"); // to use GCM
+
+		final byte[] nonce = new byte[GCM_NONCE_LENGTH];
+
+		random.nextBytes(nonce);
+
+		GCMParameterSpec spec =
+
+				new GCMParameterSpec(GCM_TAG_LENGTH * 8, nonce); // Parameters for GCM
+
+		cipher.init(Cipher.ENCRYPT_MODE, key, spec);
+
+		byte[] aad = "Tha GMAC Proof I like".getBytes();
+		;
+
+		cipher.updateAAD(aad);
+
+		byte[] cipherText = cipher.doFinal(input); // now as usual
+
+		// Decrypt; nonce is shared implicitly
+
+		cipher.init(Cipher.DECRYPT_MODE, key, spec);
+
+		// EXPECTED: Uncommenting this will cause
+
+		// an AEADBadTagException when decrypting
+
+		// because AAD value is altered
+
+		if (testNum == 1)
+			aad[1]++;
+
+		cipher.updateAAD(aad);
+
+		// EXPECTED: Uncommenting this will cause an
+
+		// AEADBadTagException when decrypting
+
+		// because the encrypted data has been altered
+
+		if (testNum == 2)
+			cipherText[10]++;
+
+		// EXPECTED: Uncommenting this will cause an
+
+		// AEADBadTagException when decrypting
+
+		// because the tag has been altered
+
+		if (testNum == 3)
+			cipherText[cipherText.length - 2]++;
+
+		try {
+
+			byte[] plainText = cipher.doFinal(cipherText);
+
+			if (testNum != 0) {
+
+				System.out.println("Test Failed: expected AEADBadTagException not thrown");
+
+			} else {
+
+				// check of decryption result
+
+				if (Arrays.equals(input, plainText)) {
+
+					System.out.println("Test Passed: match!");
+
+					String stringresult = new String(plainText);
+
+					System.out.println("Plaintext decrypted:" + stringresult);
+
+				} else {
+
+					System.out.println("Test Failed: result mismatch!");
+
+					System.out.println(new String(plainText));
+
+				}
+
+			}
+
+		} catch (AEADBadTagException ex) {
+
+			if (testNum == 0) {
+
+				System.out.println("Test Failed: unexpected ex " + ex);
+
+				ex.printStackTrace();
+
+			} else {
+
+				System.out.println("Test Passed: expected ex " + ex);
+
+			}
+
+		}
+
+	}
+
+}
