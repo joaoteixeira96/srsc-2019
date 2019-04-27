54495ee8ab3d26b4eb1ce06379fc4c73c64ed09f
joaoteixeira <joaoteixeira@joaoteixeira-X555LJ>
2019-04-27 16:28:40 +0100

    KeyStore Manager complete

---
+++ src/keyStore/KeyStoreManager.java
@@ -0,0 +1,95 @@
+package keyStore;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStore.ProtectionParameter;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+public class KeyStoreManager {
+
+	private static final char[] PASSWORD = "password".toCharArray();
+	private static final String NO_MAC_TYPE = "";
+
+	public static SecretKey getEncryptionKey(String algorithm) throws KeyStoreException, NoSuchAlgorithmException,
+			FileNotFoundException, CertificateException, IOException, UnrecoverableKeyException {
+		KeyStore keyStore = KeyStore.getInstance("JCEKS");
+		FileInputStream stream = new FileInputStream("mykeystore.jceks");
+		keyStore.load(stream, PASSWORD);
+
+		return fetchKey(keyStore, algorithm, NO_MAC_TYPE);
+	}
+
+	public static SecretKey getHashKey(String algorithm, String macType)
+			throws KeyStoreException, NoSuchAlgorithmException, FileNotFoundException, CertificateException,
+			IOException, UnrecoverableKeyException {
+		KeyStore keyStore = KeyStore.getInstance("JCEKS");
+		FileInputStream stream = new FileInputStream("mykeystore.jceks");
+		keyStore.load(stream, PASSWORD);
+		return fetchKey(keyStore, algorithm, macType);
+	}
+
+	private static SecretKey fetchKey(KeyStore keyStore, String algorithm, String macType)
+			throws NoSuchAlgorithmException, KeyStoreException, FileNotFoundException, CertificateException,
+			IOException, UnrecoverableKeyException {
+		String keyToFetch = macType.isEmpty() ? algorithm : algorithm + macType;
+		SecretKey key = (SecretKey) keyStore.getKey(keyToFetch, PASSWORD);
+		if (key == null) {
+			key = generateKey(algorithm);
+			if (!macType.isEmpty())
+				algorithm += macType;
+			storeKey(keyStore, key, algorithm);
+		}
+		return key;
+
+	}
+
+	public static void deleteOldKey() {
+
+	}
+
+	private static SecretKey generateKey(String algorithm) throws NoSuchAlgorithmException {
+		KeyGenerator generator;
+		generator = KeyGenerator.getInstance(algorithm);
+
+		switch (algorithm) {
+		case "AES":
+			generator.init(256);
+			break;
+		case "blowfish":
+			generator.init(448);
+			break;
+		case "DES":
+			generator.init(64);
+			break;
+		case "DESede":
+			generator.init(192);
+			break;
+		case "HMacSHA256":
+			generator.init(256);
+		default:
+			break;
+		}
+		return generator.generateKey();
+	}
+
+	private static void storeKey(KeyStore keyStore, SecretKey key, String algorithm) throws KeyStoreException,
+			FileNotFoundException, IOException, NoSuchAlgorithmException, CertificateException {
+		KeyStore.SecretKeyEntry secret = new KeyStore.SecretKeyEntry(key);
+		ProtectionParameter protParam = new KeyStore.PasswordProtection(PASSWORD);
+		keyStore.setEntry(algorithm, secret, protParam);
+
+		try (FileOutputStream fos = new FileOutputStream("mykeystore.jceks")) {
+			keyStore.store(fos, PASSWORD);
+		}
+	}
+
+}
